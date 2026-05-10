3f106ddcd450675869bce4b99df672d199d4e18a
Khaled Abouzeid <khaled01018797228@gmail.com>
2026-05-10 08:25:39 +0300

    save data in class added

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from helpers import transform_to_base
 
+
+
+CATEGORIES = [
+    "Food",
+    "Housing",
+    "Transportation",
+    "Health", 
+    "Education"
+]
+
+# validation
+    # if cat in CATEGORIES:
+
+    # amount=round(amount, 2)
+    # if amount > 0 :
+
+    # if currency in CURRENCIES:
+
 cash=transform_to_base("EGP", 3000)
 print(cash)
--- models.py
+++ models.py
@@ -1,7 +1,21 @@
 import sqlite3
+import sys
+
+
+
+
+
 
 class expense:
     def __init__(self, category, amount, currency):
         self.category=category
         self.amount= amount
         self.currency=currency
+
+    def save(self, amount_in_base):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO expenses (category, amount, currency, amount_in_base) VALUES (?,?,?,?)",(self.category,self.amount,self.currency, amount_in_base)  )
+        conn.commit()
+        conn.close()
+       
